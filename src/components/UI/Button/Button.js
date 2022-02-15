9e62c8ed39348f446533b35e82350570d6a59b5f
Manuel Villadiego <manuel.villadiego@arroyoconsulting.net>
2022-02-15 08:13:37 -0500

    Update Commit

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,11 @@
-import styled from 'styled-components'; // Third party library npm install --save styled-components
-
+import React from "react";
+import styles from './Button.module.css';
 /* import './Button.css'; */
 
+
+/* import styled from 'styled-components'; // Third party library npm install --save styled-components
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -10,6 +13,10 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -22,14 +29,14 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 
-`;
+`; */
 
-/* const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
-}; */
+};
 
 export default Button;
